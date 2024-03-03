e2489a2b77d41315961ac1d8448059bd7dde5864
Breno Andrade Costa <breno.ac@gmail.com>
2024-03-03 11:22:51 -0300

    atualização de dados do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,104 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+
+const estado = reactive({
+  valorA: 0,
+  valorB: 0,
+  filtro: 'Somar',
+  tarefas: [
+    {
+      titulo: 'Somar',
+      finalizada: true,
+    },
+    {
+      titulo: 'Subtrair',
+      finalizada: false,
+    },
+    {
+      titulo: 'Multiplicar',
+      finalizada: false,
+    },
+    {
+      titulo: 'Dividir',
+      finalizada: false,
+    }
+
+
+  ]
+})
+
+function tarefaSomar() {
+  const { valorA, valorB } = estado;
+  return parseInt(valorA) + parseInt(valorB);
+}
+function tarefaSubtrair() {
+  const { valorA, valorB } = estado;
+  return parseInt(valorA) - parseInt(valorB);
+}
+function tarefaMultiplicar() {
+  const { valorA, valorB } = estado;
+  return parseInt(valorA) * parseInt(valorB);
+}
+function tarefaDividir() {
+  const { valorA, valorB } = estado;
+  return parseInt(valorA) / parseInt(valorB);
+}
+
+const getOperadores = () => {
+  const { filtro } = estado;
+
+  switch (filtro) {
+    case 'Somar':
+      return tarefaSomar();
+    case 'Subtrair':
+      return tarefaSubtrair();
+    case 'Multiplicar':
+      return tarefaMultiplicar();
+    case 'Dividir':
+      return tarefaDividir();
+    default:
+      return estado.tarefas
+  }
+
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <header class="mb-3 mt-5 bg-secondary rounded-3 text-white d-flex justify-content-center">
+      <h1>Calculadora Aritimética</h1>
+    </header>
+    <form>
+      <div class="row">
+        <div class="col">
+          <input @keyup="evento => estado.valorA = evento.target.value" type="number" placeholder="Digite aqui o Valor A"
+            class="form-control" id="valorA">
+        </div>
+        <select @change="evento => estado.filtro = evento.target.value" class="col-md-2 rounded-3">
+          <option value="+">Somar</option>
+          <option value="-">Subtrair</option>
+          <option value="*">Multiplicar</option>
+          <option value="/">Dividir</option>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+        </select>
+        <div class="col">
+          <input @keyup="evento => estado.valorB = evento.target.value" type="number" placeholder="Digite aqui o Valor B"
+            class="form-control" id="valorB">
+        </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+        <div class="row d-flex justify-content-center">
+          <div class="col-md-3">
+            <h3 class="bg-light mt-3 border border-secondary rounded-3">{{ getOperadores () }}</h3>
+            
+          </div>
+        </div>
+
+      </div>
+    </form>
+
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
